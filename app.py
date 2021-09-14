3c7cb971c9baf17055bee372f56a69ed4ea6e88f
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-09-14 15:11:41 -0400

    fix?

--- app.py
+++ app.py
@@ -4,14 +4,11 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234567890'
 socketio = SocketIO(app)
-
-#@socketio.on('connect')
-#def show_connect_status(data):
-#	print(f'Connected: {data!r}')
+socketio.init_app(app, cors_allowed_origins="*")
 
 @socketio.on('message_send')
 def send_message(data):
-	print('RECIEVED ' + data)
+	print('RECIEVED ' + data['data'])
 	username = session.get('username', 'unknown')
 	data['data'] = username + ': ' + data['data']
 	emit('new_message', data, broadcast=True)
@@ -26,3 +23,6 @@ def change_username(data):
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+if __name__ == '__main__':
+	socketio.run(app)
